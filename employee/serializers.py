e91fb50c87c46bdb99ad8cdae11970a61ea04c3d
Jakob Damgaard Møller <jakobdo@gmail.com>
2020-06-25 13:04:49 +0200

    Technology Level added

--- employee/serializers.py
+++ employee/serializers.py
@@ -1,11 +1,24 @@
 from rest_framework import serializers
 from stdimage_serializer.fields import StdImageField
 
-from employee.models import Employee
+from employee.models import Employee, TechnologyLevel
+
+
+class TechnologyLevelSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source='technology.id')
+    name = serializers.ReadOnlyField(source='technology.name')
+
+    class Meta:
+        model = TechnologyLevel
+        fields = ['id', 'name', 'level']
 
 
 class ConsultantSerializer(serializers.ModelSerializer):
     image = StdImageField()
+    technologies = TechnologyLevelSerializer(
+        source='technologylevel_set',
+        many=True
+    )
 
     class Meta:
         model = Employee
