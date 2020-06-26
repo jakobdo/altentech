7c1718ee4dca426699590b381b2ec8a6386fe9f4
Jakob Damgaard Møller <jakobdo@gmail.com>
2020-06-26 07:10:24 +0200

    Consultants are returned random ordered

--- employee/viewsets.py
+++ employee/viewsets.py
@@ -1,9 +1,17 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from employee.models import Employee
 from employee.serializers import ConsultantSerializer
 
 
 class ConsultantViewSet(viewsets.ModelViewSet):
-    queryset = Employee.objects.all()
+    queryset = Employee.objects.order_by('?')
     serializer_class = ConsultantSerializer
+
+    def retrieve(self, request, pk):
+        if pk == '0':
+            instance = self.queryset.first()
+            serializer = self.get_serializer(instance)
+            return Response(serializer.data)
+        return super().retrieve(request, pk)
