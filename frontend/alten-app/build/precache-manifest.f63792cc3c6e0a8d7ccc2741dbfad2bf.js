2b9f6e9ec8e09d87093668bbfdd375c29da26c05
Jakob Damgaard Møller <jakobdo@gmail.com>
2020-07-06 19:57:49 +0200

    react build included

---
+++ frontend/alten-app/build/precache-manifest.f63792cc3c6e0a8d7ccc2741dbfad2bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0befa3ced9ea01ca7ca5dd692dd2cef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1177ce5a7b8a9cc11da3",
+    "url": "/static/css/main.583505ca.chunk.css"
+  },
+  {
+    "revision": "2db7dfd1ba5356972d4b",
+    "url": "/static/js/2.a80ee156.chunk.js"
+  },
+  {
+    "revision": "8debe1713c30c1c8fa79b57451984727",
+    "url": "/static/js/2.a80ee156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1177ce5a7b8a9cc11da3",
+    "url": "/static/js/main.f9a91245.chunk.js"
+  },
+  {
+    "revision": "0a3e8cb2d2665185ac1b",
+    "url": "/static/js/runtime-main.88e329da.js"
+  }
+]);
